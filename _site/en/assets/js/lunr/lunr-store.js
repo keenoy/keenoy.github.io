2ce4029dc84ce38f28e145e3a04d442695a8ef4d
Keenoy <yths21@gmail.com>
2025-08-24 14:54:57 +0900

    Sun, Aug 24, 2025  2:54:56 PM

---
+++ _site/en/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Skipping Bunnies",
+        "excerpt":"Under Preparation  ","categories": [],
+        "tags": [],
+        "url": "/game/skipping-bunnies/",
+        "teaser": "https://placehold.co/500x250?text=No+Image"
+      },{
+        "title": "Tetropow",
+        "excerpt":"Under Preparation  ","categories": [],
+        "tags": [],
+        "url": "/game/tetropow/",
+        "teaser": "/assets/images/tetropow/logo.png"
+      },{
+        "title": "Under Preparation",
+        "excerpt":"Under Preparation  ","categories": ["post"],
+        "tags": [],
+        "url": "/post/2025/08/21/temp/",
+        "teaser": null
+      }]
